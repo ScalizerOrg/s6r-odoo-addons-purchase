2319237c15a3fcb72696599d58ca3c9692d64f65
Houda <houda.bentaleb@gmail.com>
2026-04-03 18:01:15 +0100

    [ADD]s6r_purchase_my_documents_only

---
+++ s6r_purchase_my_documents_only/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2026 Scalizer (<https://www.scalizer.fr>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from .hooks import post_init_hook
---
+++ s6r_purchase_my_documents_only/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2026 Scalizer (<https://www.scalizer.fr>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Scalizer Purchase - My Documents Only",
+    "version": "19.0.1.0.0",
+    "summary": "Restrict purchase users to their own purchase documents and related vendor bills",
+    "category": "Purchases",
+    "author": "Scalizer",
+    "website": "https://www.scalizer.fr",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        "purchase_requisition",
+        "spreadsheet_dashboard_purchase_stock",
+    ],
+    "data": [
+        "security/purchase_my_documents_groups.xml",
+        "security/ir.model.access.csv",
+        "security/purchase_my_documents_rules.xml",
+        "views/purchase_menu_views.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "application": False,
+}
